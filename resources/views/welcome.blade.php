a951930dba85799c7b55c744f62c73f88f9b0211
Daniel Alemany <dalpe2003@gmail.com>
2024-12-05 00:00:57 +0100

    pruebaaaagjgyseiofsjfdawawdadi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="container mx-auto mt-4">
-    <h1 class="text-3xl font-bold text-blue-800">Bienvenido a la Guia de Futbol Femeníaaa</h1>
+    <h1 class="text-3xl font-bold text-blue-800">Bienvenido a la Guia de Futbol Femení</h1>
 </div>
 
 
