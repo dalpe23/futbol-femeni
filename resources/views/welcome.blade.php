13b4d6d4c37bcd18fbe5f113351bdd0a56bcd72d
Miguel Angel Martinez <tartanasdev@gmail.com>
2024-12-05 00:14:35 +0100

    A

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container mx-auto mt-4">
     <h1 class="text-3xl font-bold text-blue-800">Bienvenido a la Guia de Futbol Femení</h1>
-    <h2>A</h2>
+    <h2>Miguel Angel Martinez</h2>
 </div>
 
 
